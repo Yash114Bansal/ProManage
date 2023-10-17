b0db011da26744b7b084ecc7db6818643446aaa9
yash114bansal <yash114bansal@gmail.com>
2023-10-17 16:57:12 +0530

    add token grant

---
+++ authentication/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView,TokenVerifyView
+
+urlpatterns = [
+    path("generate/",TokenObtainPairView.as_view()),
+    path("verify",TokenVerifyView.as_view()),
+    path("refresh/",TokenRefreshView.as_view()),
+]
