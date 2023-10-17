f1c0f39eebc390afb7b4a8cb9be6dac9051cd775
yash114bansal <yash114bansal@gmail.com>
2023-10-17 17:23:08 +0530

    add authentication and authorization

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
-# Create your views here.
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAdminUser
+from rest_framework_simplejwt.authentication import JWTAuthentication
+class createUser(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAdminUser]
+    authentication_classes = [JWTAuthentication]
